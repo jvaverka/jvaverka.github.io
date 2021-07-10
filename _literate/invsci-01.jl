aa6a0503aaf5bf494d9e927f15b20572412fe889
JVaverka <jvaverka@protonmail.com>
2021-07-10 11:20:58 -0400

    try to suppress package build info again

--- _literate/invsci-01.jl
+++ _literate/invsci-01.jl
@@ -103,13 +103,13 @@ show(df)
 maximum(values)
 #+
 using PyPlot;
-figure(figsize=(4, 2));
+figure(figsize=(8, 6));
 plot(df.year, df.revenue);
 
 xlabel("Year", fontsize=14);
 ylabel("Revenue", fontsize=14);
 
-plt.savefig(joinpath(@OUTPUT, "invsci-01-time-to-harvest.svg")) # hide
+plt.savefig(joinpath(@OUTPUT, "invsci-01-time-to-harvest.svg")); # hide
 
 # \figalt{Time to Harvest}{invsci-01-time-to-harvest.svg}
 #
