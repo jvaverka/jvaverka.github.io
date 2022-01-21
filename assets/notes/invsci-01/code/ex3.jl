3b3a3e268c66ded26dd0bc9650557c6f79f50229
jvaverka <47707103+jvaverka@users.noreply.github.com>
2022-01-21 03:22:30 +0000

    Deploying to gh-pages from  @ c1933322a96665811d32efaf05bae92308f5100e 🚀

--- assets/notes/invsci-01/code/ex3.jl
+++
@@ -1,4 +0,0 @@
-# This file was generated, do not modify it. # hide
-using DataFrames
-df = DataFrame(Dict(:year => 1:20, :revenue => values))
-show(df)
--- assets/notes/invsci-01/code/ex5.jl
+++
@@ -1,9 +0,0 @@
-# This file was generated, do not modify it. # hide
-using PyPlot;
-figure(figsize=(8, 6));
-plot(df.year, df.revenue);
-
-xlabel("Year", fontsize=14);
-ylabel("Revenue", fontsize=14);
-
-plt.savefig(joinpath(@OUTPUT, "invsci-01-time-to-harvest.svg")); # hide
